3cafcb440d7fbda916d3509846cf02aaed8cac5f
AMRITHA LAL <lalamritha42@gmail.com>
2022-09-29 10:17:03 +0530

    commit

--- bin/factorial.dart
+++ bin/factorial.dart
@@ -1,11 +1,10 @@
 import 'dart:io';
-
 void main(){
   int i,fac=1;
   print("Enter the number = ");
-  int n = int.parse(stdin.readLineSync()!);
+ int n = int.parse(stdin.readLineSync()!);
   for(i=1;i<=n;i++){
-    fac=fac * i ;
+    fac = fac*i;
   }
   print('Factorial of $n = $fac');
 
---
+++ bin/return.dart
@@ -0,0 +1,14 @@
+void sum(){
+  int a=10,b=20,c=30;
+  int result=a+b+c;
+  print(result);
+}
+int diff(){
+  int out=240-40;
+  return out;
+}
+void main(){
+  sum();
+  int outab= diff();
+  print(outab);
+}
